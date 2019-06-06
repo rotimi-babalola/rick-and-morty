8d8c74e17555a083ceb707a7607c47cdd5bfea5f
rotimi-babalola <rotimi.babalola@andela.com>
2019-06-06 11:08:25 +0100

    use apollo for fetching

--- client/src/components/Characters.jsx
+++ client/src/components/Characters.jsx
@@ -1,44 +1,28 @@
 import React from 'react';
+import { Query } from 'react-apollo';
 
-import useRickMortyApi from '../hooks/useRickMortyApi';
 import getSearchQuery from '../utils/getSearchQuery';
 
 import Loading from './Loading';
-import Card from './Card';
 
 import '../styles/characters.scss';
 
-const initialData = { characters: { info: {}, results: [] } };
+const GET_CHARACTERS_QUERY = getSearchQuery();
 
-const CHARACTERS_QUERY = getSearchQuery();
-
-const Characters = () => {
-  const { data, isLoading, isError } = useRickMortyApi(
-    CHARACTERS_QUERY,
-    initialData,
-  );
-
-  if (isError) {
-    return <h1>An error occurred</h1>;
-  }
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  return (
-    <div className="card-wrapper">
-      {data.characters.results.map(el => (
-        <Card
-          key={el.id}
-          imgSrc={el.image}
-          name={el.name}
-          species={el.species}
-          gender={el.gender}
-        />
-      ))}
-    </div>
-  );
+const CardList = props => {
+  console.log(props, '>>>');
+  return <h1>kdkd</h1>;
 };
+
+const Characters = () => (
+  <Query query={GET_CHARACTERS_QUERY}>
+    {({ data, loading }) => {
+      if (loading || !data) {
+        return <Loading />;
+      }
+      return <CardList data={data} />;
+    }}
+  </Query>
+);
 
 export default Characters;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { API_URL } from './constants';
+
 import App from './components/App';
 
-render(<App />, document.getElementById('app'));
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
+  uri: API_URL,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'),
+);
